196341cdf854f553c294a7ae0406a7ae1052f5ba
GetSetSold <rohit@getsetsold.com>
2025-07-15 00:22:06 -0400

    Update fetchAndSave.js

--- fetchAndSave.js
+++ fetchAndSave.js
@@ -144,6 +144,7 @@ function mapProperties(properties, officeDetails) {
       UnparsedAddress: property.UnparsedAddress,
       PostalCode: property.PostalCode,
       SubdivisionName: property.SubdivisionName,
+      CommunityName: property.CommunityName,
       UnitNumber: property.UnitNumber,
       City: property.City,
       Directions: property.Directions,
