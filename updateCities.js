b3bebe8a1bcf91a0e5d5a70ee654c7e6ed552451
GetSetSold <rohit@getsetsold.com>
2025-09-01 16:51:39 -0400

    Update updateCities.js

--- updateCities.js
+++ updateCities.js
@@ -1,8 +1,8 @@
 import fetch from "node-fetch";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseUrl = 'https://nkjxlwuextxzpeohutxz.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 async function updateCities() {
